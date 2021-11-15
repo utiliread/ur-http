2a8986f1150e36849ea4582d40fa0ddad1a28444
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2021-11-15 14:46:52 +0100

    Add non array expectMessagePack

--- src/msgpack.ts
+++ src/msgpack.ts
@@ -1,12 +1,15 @@
 import { deserialize } from "@utiliread/msgpack";
 import { HttpBuilder } from "./http-builder";
-import { decodeArrayStream } from "@msgpack/msgpack";
+import { decodeArrayStream, decodeAsync } from "@msgpack/msgpack";
 import { getMapper, Mapper, Type } from "./mapping";
 
 type TypeOrMapper<T> = Type<T> | Mapper<T>;
 
 declare module "./http-builder" {
   interface HttpBuilder {
+    expectMessagePack<T>(
+      typeOrMapper?: TypeOrMapper<T>
+    ): HttpBuilderOfT<T>;
     expectMessagePackArray<T>(
       typeOrMapper?: TypeOrMapper<T>
     ): HttpBuilderOfT<T[]>;
@@ -15,6 +18,18 @@ declare module "./http-builder" {
     ): HttpBuilderOfT<AsyncGenerator<T, void, unknown>>;
   }
 }
+
+HttpBuilder.prototype.expectMessagePack = function <T>(
+  this: HttpBuilder,
+  typeOrMapper?: TypeOrMapper<T>
+) {
+  this.message.headers.set("Accept", "application/x-msgpack");
+  return this.useHandler(async (response) => {
+    const itemFactory = getMapper(deserialize, typeOrMapper);
+    const decoded = decodeAsync(response.body!);
+    return itemFactory(decoded);
+  });
+};
 
 HttpBuilder.prototype.expectMessagePackArray = function <T>(
   this: HttpBuilder,
