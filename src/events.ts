f4006bcb2207c15eb6788ae6fd2bd17a77431851
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2022-01-11 08:39:31 +0100

    Add events extension

---
+++ src/events.ts
@@ -0,0 +1,35 @@
+import { HttpBuilder, HttpBuilderOfT, Message } from "./http-builder";
+import { HttpResponse } from "./http-response";
+
+interface Events {
+  sent: (response: HttpResponse, request: Message) => void | Promise<void>;
+}
+
+interface EventsOfT<T> extends Events {
+  received: (
+    response: HttpResponse,
+    request: Message,
+    value: T
+  ) => void | Promise<void>;
+}
+
+export function events<B extends HttpBuilder, P extends any[]>(
+  action: (...params: P) => B,
+  configure: (...params: P) => Partial<Events>
+): (...params: P) => B;
+export function events<B extends HttpBuilderOfT<T>, P extends any[], T>(
+  action: (...params: P) => B,
+  configure: (...params: P) => Partial<EventsOfT<T>>
+): (...params: P) => B {
+  return function (...params: P) {
+    const builder = action(...params);
+    const events = configure(...params);
+    if (events.sent) {
+      builder.onSent(events.sent);
+    }
+    if (builder instanceof HttpBuilderOfT && events.received) {
+      builder.onReceived(events.received);
+    }
+    return builder;
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,10 @@ export {
 export { ProblemDetails } from "./problem-details";
 export { QueryString } from "./query-string";
 export { HttpResponse, HttpResponseOfT } from "./http-response";
-export { HttpBuilder, HttpBuilderOfT } from "./http-builder";
+export { HttpBuilder, HttpBuilderOfT, Message } from "./http-builder";
 export { HttpError } from "./http-error";
 export { TimeoutError } from "./timeout-error";
+export { events } from "./events";
 export * from "./helpers";
 export { Fetch } from "./http";
 export { Subscription } from "./event-aggregator";
